61839a7d60bb05d874ae8246acd7981a8791530b
Giulia Orrù <151760540+giuliaaorru@users.noreply.github.com>
2023-11-22 16:04:19 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import logo from "./res/logo.svg";
 
 //Dichiarazione Styled Components:
-const Div = styled.div`
-  margin: 100px auto;
-  padding: 50px;
-  background-color: #66A182;
-  border: 2px solid #2E4057;
-  max-width: 700px;
-  border-radius: 5px;
+const Container = styled.div`
   display: flex;
   flex-direction: column;
 `;
+const Navbar = styled.div`
+  background-color: #1d1d1d;
+  display: flex;
+  justify-content: space-between;
+`;
 
-const H1 = styled.h1`
-  color: #2E4057;
+const Logo = styled.img`
+  width: 3rem;
   margin: auto;
+  margin-left: 0;
+  filter: invert(1);
+`;
+
+const ElementContent = styled.a`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  color: white;
+  padding: 5px 20px 5px 20px;
+`;
+
+const Title = styled.h1`
+  width: 75%;
+  margin: auto;
+  margin-bottom: 0;
   text-align: center;
-  font-size: 70px;
+  font-size: 4rem;
+`;
+
+const Description = styled.h3`
+  width: 75%;
+  margin: 5px auto;
+  text-align: center;
+  font-size: 2rem;
+  color: black;
 `;
 
 const App = () => {
-
-  return(
-    <Div>
-      <H1>Hello world</H1>
-    </Div>
-  )
+  return (
+    <Container>
+      <Navbar>
+        <Logo src={logo} />
+        <ElementContent> Me </ElementContent>
+      </Navbar>
+      <Title>ciao!</Title>
+      <Description>Mi chiamo Giulia</Description>
+    </Container>
+  );
 };
 
 export default App;
