92661da6c79a25b3a9f38b5e467636160f29d9bf
Giulia Orrù <151760540+giuliaaorru@users.noreply.github.com>
2023-11-29 16:22:57 +0000

    Update 10 files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Education from "./components/Education";
 
 import logo from "./res/logo.svg";
+import photo1 from "./res/photo1.svg";
+import photo2 from "./res/photo2.svg";
 
 //Dichiarazione Styled Components:
 const Container = styled.div`
@@ -9,7 +12,7 @@ const Container = styled.div`
   flex-direction: column;
 `;
 const Navbar = styled.div`
-  background-color: #1d1d1d;
+  background-color: #9f3699;
   display: flex;
   justify-content: space-between;
 `;
@@ -21,12 +24,27 @@ const Logo = styled.img`
   filter: invert(1);
 `;
 
+const DivHome = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  padding-top: 50px;
+  padding: 10px;
+  align-items: center;
+`;
+
 const ElementContent = styled.a`
   text-decoration: none;
   font-size: 1.5rem;
   font-weight: 400;
   color: white;
   padding: 5px 20px 5px 20px;
+`;
+
+const DivScritte = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
 `;
 
 const Title = styled.h1`
@@ -45,6 +63,24 @@ const Description = styled.h3`
   color: black;
 `;
 
+const Name = styled.a`
+  color: #9f3699;
+`;
+
+const Photo = styled.img`
+  display: flex;
+  margin-bottom: 30px;
+  width: 60%;
+  max-width: 500px;
+  margin: auto;
+`;
+
+const ContainerIcon = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: auto;
+`;
+
 const App = () => {
   return (
     <Container>
@@ -52,8 +88,21 @@ const App = () => {
         <Logo src={logo} />
         <ElementContent> Me </ElementContent>
       </Navbar>
-      <Title>ciao!</Title>
-      <Description>Mi chiamo Giulia</Description>
+      <DivHome>
+        <DivScritte>
+          <Title>Ciao!</Title>
+          <Description>
+            Mi chiamo
+            <Name> Giulia </Name>e sto seguendo il corso di curvatura
+            informatica.
+          </Description>
+        </DivScritte>
+        <Photo src={photo1} />
+      </DivHome>
+      <DivHome>
+        <Photo src={photo2} />
+        <Education />
+      </DivHome>
     </Container>
   );
 };
